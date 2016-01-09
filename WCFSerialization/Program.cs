d58e914cb34869f04f4faf809ad3a820e3e10249
kelvinlei <942738793@qq.com>
2016-01-09 18:28:08 +0800

    WCF中的序列化-DataContractSerializer
    DataContractSerializer通过Mapping方式将.NET Object转化为XML

---
+++ WCFSerialization/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WCFSerialization
+{
+    class Program
+    {
+        static string _basePath = @"E:\gitRepo\WCFService_Demo\WCFSerialization\";
+
+        static void Main(string[] args)
+        {
+            SerializeViaDataContractSerializer();
+
+            DeserializeViaDataContractSerializer();
+        }
+
+        static void SerializeViaDataContractSerializer()
+        {
+            DataContractProduct product = new DataContractProduct(Guid.NewGuid(), "Dell PC", "Xiamen FuJian", 4500);
+            DataContractOrder order = new DataContractOrder(Guid.NewGuid(), DateTime.Today, product, 300);
+            string fileName = _basePath + "Order.DataContractSerializer.xml";
+            using (FileStream fs = new FileStream(fileName, FileMode.Create))
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(DataContractOrder));
+                using (XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(fs))
+                {
+                    serializer.WriteObject(writer, order);
+                }
+            }
+            Process.Start(fileName);
+        }
+
+        static void DeserializeViaDataContractSerializer()
+        {
+            string fileName = _basePath + "Order.DataContractSerializer.xml";
+            DataContractOrder order;
+            using (FileStream fs = new FileStream(fileName, FileMode.Open))
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(DataContractOrder));
+
+                using (XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas()))
+                {
+                    order = serializer.ReadObject(reader) as DataContractOrder;
+                }
+            }
+
+            Console.WriteLine(order);
+            Console.Read();
+        }
+    }
+}
