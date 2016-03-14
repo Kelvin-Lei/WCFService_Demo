459ff7a5cb5254027b7bcf06235f9c0bed32d6fa
kelvinlei <942738793@qq.com>
2016-03-14 19:49:41 +0800

    通过WCF Extension实现Localization
    通过WCF Extension实现Localization

---
+++ Messages.Service/MessageService.cs
@@ -0,0 +1,18 @@
+﻿using Messages.Contract;
+using Messages.Service.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages.Service
+{
+    public class MessageService : IMessage
+    {
+        public string GetMessage()
+        {
+            return Resources.HelloWorld;
+        }
+    }
+}
